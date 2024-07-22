b0374b9ff45844eacf8d744bea0cb1cb0449b71c
Vinícius <vinicius.silva120@gmail.com>
2024-07-22 10:53:01 -0300

    feat: Add POST method
    It adds the POST method. It creates a new task.

---
+++ app.py
@@ -0,0 +1,26 @@
+from typing import List
+
+from flask import Flask, request, jsonify
+
+from models.task import Task
+
+# __name__ == __main__
+app = Flask(__name__)
+
+tasks: List = []
+task_id_control = 1
+
+
+@app.route('/tasks', methods=['POST'])
+def create_task():
+    global task_id_control
+    data = request.get_json()
+    new_task = Task(id=task_id_control, title=data.get('title'), description=data.get('description', ""))
+    task_id_control += 1
+    tasks.append(new_task)
+    print(tasks)
+    return jsonify({"message": "Nova tarefa criada com sucesso", "id": new_task.id})
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
